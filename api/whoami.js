069b3c8f80eed0a323d3a2eac76dbafb4b5da5c2
aravmehta20 <aravm0720@gmail.com>
2025-11-11 22:19:05 -0600

    Update whoami.js

--- api/whoami.js
+++ api/whoami.js
@@ -1,7 +1,1 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    ui_url: process.env.UI_URL || null,
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI || null,
-    vercel_url: process.env.VERCEL_URL || null
-  });
-}
+
