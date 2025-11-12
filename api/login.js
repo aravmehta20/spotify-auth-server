28f90d4c5c567efb2dcc2b523b50e2dfe4edf2db
aravmehta20 <aravm0720@gmail.com>
2025-11-11 18:02:44 -0600

    Update login.js

--- api/login.js
+++ api/login.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 export default function handler(req, res) {
   const client_id = process.env.SPOTIFY_CLIENT_ID;
   const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
-  const scope = 'user-read-playback-state user-modify-playback-state playlist-read-private';
+  const scope = 'streaming user-read-email user-read-private user-modify-playback-state user-read-playback-state';
   const state = Math.random().toString(36).substring(2, 15);
 
   const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}`;
